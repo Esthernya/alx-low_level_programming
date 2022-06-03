b78af5ce2fd61431f8c8a59e3228c12904f71e91
Esthernya <esthernya29.en@gmail.com>
2022-06-02 19:20:34 -0700

    Task 4.

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,15 @@
+#include <stdio.h>#include <stdio.h>
+/**
+ *  * main should print out a string at entry.
+ *   * Return: 0 if success.
+ *    */
+int main(void)
+{
+		puts("\"Programming is like building a multilingual puzzle");
+			return (0);
+}
+int main(void)
+{
+		puts("\"Programming is like building a multilingual puzzle");
+			return (0);
+}
